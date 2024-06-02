14b8cf5b79f3565a58b44d5718a43de297d9aa80
santiago <everardoorozcocarmona@gmail.com>
2024-06-02 13:05:57 -0500

    feat: se hizo una acomodacion en la subasta

--- src/presentation/controllers/subastas/subasta.controller.ts
+++ src/presentation/controllers/subastas/subasta.controller.ts
@@ -1,17 +1,17 @@
 import { AppDataSource } from "../../data-source";
 
 
-export class SubastaController{
+export class SubastaController {
 
-    async usuariosEnLinea(req:any, res:any){
-        const totalDeUsuarios = AppDataSource.query(
-            'select count(*) as usuarios_en_linea from subastas where estado = 1;'            
+    async usuariosEnLinea(req: any, res: any) {
+        const totalDeUsuarios = await AppDataSource.query(
+            'select count(*) as usuarios_en_linea from subastas where estado = 1;'
         );
 
         return res.status(200).json({
             status: 'success',
-            message: totalDeUsuarios
+            message: Number(totalDeUsuarios[0].usuarios_en_linea)
         });
-        
+
     }
 }
