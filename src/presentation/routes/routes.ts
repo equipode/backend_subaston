9b14f5cb6b12ff405eefc0a94dc30ac604e63ea2
Robert López oviedo <ingenierolopez2022@gmail.com>
2024-06-02 12:57:13 -0500

    feat; creación de empoint para traer los usuarios en linea

--- src/presentation/routes/routes.ts
+++ src/presentation/routes/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authMildleware } from "../middlewares/auth.middlewar";
 import { AuthRoutes } from "./auth/routes_auth";
 import { ProductoRoutes } from "./productos/routes_product";
+import { SubastaRoutes } from "./subastas/routes_subasta";
 
 
 export class AppRoutes {
@@ -13,6 +14,7 @@ export class AppRoutes {
         //Definir Rutas principales
         router.use('/api/v1/auth', AuthRoutes.routes);
         router.use('/api/v1/producto', authMildleware, ProductoRoutes.routes);
+        router.use('/api/v1/subasta', authMildleware, SubastaRoutes.routes);
         return router;
     }
 
